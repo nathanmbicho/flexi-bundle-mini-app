8bf74ba611939d7de1aca26bb596637fb41ff615
Nathan M <nathanmbicho@gmail.com>
2025-03-18 14:25:00 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Flexi Bundle Mini App -  Group 4",
+  title: "Flexi Bundle Mini App -  By Nate",
   description: "Create your custom data bundle purchase",
 };
 
